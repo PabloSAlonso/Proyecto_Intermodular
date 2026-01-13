ca85e699eda373f2802d5a36bc107e6ef668b41e
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-01-13 13:54:09 +0100

    changes

--- app/src/main/java/com/example/appmovil/InicioSesion.java
+++ app/src/main/java/com/example/appmovil/InicioSesion.java
@@ -1,6 +1,8 @@
 package com.example.appmovil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.ScrollView;
@@ -13,9 +15,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class InicioSesion extends AppCompatActivity {
     EditText etMail,etPass;
-    TextView tvMail, tvPass, tvBienvenido, tvFoot;
+    TextView tvMail, tvPass, tvBienvenido, tvFoot, tvRegistro;
     RatingBar rb;
     Toolbar tb;
     ScrollView sv;
@@ -23,11 +25,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inicio_sesion);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        tvRegistro = findViewById(R.id.tvRegistro);
+
+
+        tvRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent recoger_nuevo_user = new Intent(InicioSesion.this, RegistroCuenta.class);
+                startActivity(recoger_nuevo_user);
+            }
         });
     }
 }
