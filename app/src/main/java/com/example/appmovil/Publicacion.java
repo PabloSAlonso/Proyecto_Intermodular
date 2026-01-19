1c27ba5546e2016e7db82834b536802e025a2f65
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-01-19 18:20:53 +0100

    fix Final_Documentation separated of Weekly documentation / feat Publicacion.java class created

--- app/src/main/java/com/example/appmovil/Publicacion.java
+++ app/src/main/java/com/example/appmovil/Publicacion.java
@@ -1,4 +1,20 @@
 package com.example.appmovil;
 
-public class Publicacion {
+import android.media.Image;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Publicacion implements Serializable {
+    private int id_publicacion;
+    private int id_usuario;
+    private Date fecha_publicacion;
+
+    private Image imagen;
+
+    private String descripcion;
+    private int likes;
+    private int comentarios;
+
+
 }
