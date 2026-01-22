46431d9ac5e112a1ab528de42ea3edfd07aa0cbb
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-01-22 13:53:43 +0100

    changes

--- app/src/main/java/com/example/appmovil/Adapters/AdaptadorInicio.java
+++ app/src/main/java/com/example/appmovil/Adapters/AdaptadorInicio.java
@@ -1,7 +1,13 @@
 package com.example.appmovil.Adapters;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,10 +16,31 @@ import com.example.appmovil.Models.Publicacion;
 
 import java.util.ArrayList;
 
-public class AdaptadorInicio {
+public class AdaptadorInicio extends RecyclerView.Adapter<AdaptadorInicio.InicioViewHolder>{
     ArrayList<Publicacion> publicaciones = new ArrayList<>();
-    public class InicioViewHolder extends RecyclerView.ViewHolder{
 
+    @NonNull
+    @Override
+    public InicioViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InicioViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class InicioViewHolder extends RecyclerView.ViewHolder{
+        private TextView tvLikes, tvComentarios, tvCompartidos, tvDescripcion;
+        private ImageButton ibLikes, ibComentarios, ibCompartidos, ibPerfil, ibOpciones;
+        private ImageView ivFotoPublicacion;
+        private Toolbar tb;
+        private ScrollView sv;
         public InicioViewHolder(@NonNull View itemView) {
             super(itemView);
 
