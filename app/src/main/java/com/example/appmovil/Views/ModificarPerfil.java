d277fe29e6163c67de85cffcee0bf4a4ad57091a
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-02-04 18:01:33 +0100

    feat Modify Profile layout

--- app/src/main/java/com/example/appmovil/Views/ModificarPerfil.java
+++ app/src/main/java/com/example/appmovil/Views/ModificarPerfil.java
@@ -1,4 +1,4 @@
-package com.example.appmovil;
+package com.example.appmovil.Views;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.appmovil.R;
 
 public class ModificarPerfil extends AppCompatActivity {
 
