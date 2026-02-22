79574c546248e2d39b1aaebc2dea0e173714a339
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-02-22 10:31:36 +0100

    Refactored code in Apirest and AppMovil projects

--- app/src/main/java/com/example/appmovil/Login.java
+++ app/src/main/java/com/example/appmovil/Login.java
@@ -1,8 +1,6 @@
-package com.example.bettr;
+package com.example.appmovil;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,9 +15,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.bettr.ApiRest.Api_Gets;
-import com.example.bettr.Dao.User;
-import com.example.bettr.UserSession;
+import com.example.appmovil.ApiRest.Api_Gets;
+import com.example.appmovil.Dao.User;
+import com.example.appmovil.UserSession;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -157,4 +155,3 @@ public class Login extends AppCompatActivity {
         }
     }
 }
-
